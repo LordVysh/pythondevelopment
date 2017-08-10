e413b3d398ed6aa0ba212c4203d88e4b4e541e31
Jon Payne <lordvysh@gmail.com>
2017-08-10 14:31:12 -0500

    Multiple input baby

---
+++ vector_input.py
@@ -0,0 +1,9 @@
+def printAny(*what):
+    if what:
+        print("There are ", len(what), " arguments")
+        for thing in what[0:]:
+            print(thing)
+        print("Done printing...")
+
+printAny("Printing with three arguments:", "This thing", "Another thing")
+printAny("Only printing this")
